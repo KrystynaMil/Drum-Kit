bac24f91415bdc9075a410cbdbf38d0b76fa7995
KrystynaMil <66964490+KrystynaMil@users.noreply.github.com>
2020-12-11 09:42:52 +0100

    Create init.js

---
+++ src/init.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// import all listener files so they can attach to the DOM
+import './listeners/play-sound.js';
+import './listeners/remove-transition.js';
+
+// does your project have code that executes when the document or window are ready?
+// that code can go here
